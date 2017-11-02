620bf5a49376028c02f35e5a99bca3abc9ebdd9c
Brady Wright <pro75357@gmail.com>
2017-11-02 15:58:16 -0700

    trying

--- imports/DataCalls.js
+++ imports/DataCalls.js
@@ -1,6 +1,34 @@
 import { Session } from 'meteor/session'
 
+var mkFhir = require('fhir.js');
+
 Meteor.methods({
+    'fhir': function () {
+        var mkFhir = require('fhir.js');
+        var config = Meteor.user().config
+        var client = mkFhir(config);
+
+        client
+            .search({ type: 'Observation', query: { 'Temperature': '37' } })
+            .then(function (res) {
+                var bundle = res.data;
+                var count = (bundle.entry && bundle.entry.length) || 0;
+                console.log("# Patients born in 1974: ", count);
+                return bundle
+            })
+            .catch(function (res) {
+                //Error responses
+                if (res.status) {
+                    console.log('Error', res.status);
+                }
+
+                //Errors
+                if (res.message) {
+                    console.log('Error', res.message);
+                }
+            });
+
+    },
     'getData': function (resource, option) {
         if (resource === 'patient') {
             // Using the Oauth data in Meteor.user(), we need to make an HTTP call to fetch the patient resource. 
